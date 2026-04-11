ec85198a9b026186c0322284a5966c82d81c0905
DayannaCoral19 <dayannacor121@gmail.com>
2026-04-11 14:53:35 -0500

    Delete 05032026/Codigo directory

--- 05032026/Codigo/ejercicio3.js
+++
@@ -1,9 +0,0 @@
-// EJERCICIO 3: Contador de vidas
-let vidas = 3;
-
-console.log("Vidas restantes: " + vidas);
-
-// Pierde una vida
-vidas = vidas - 1;   // también podría ser vidas-- o vidas -= 1
-
-console.log("Vidas restantes: " + vidas);
